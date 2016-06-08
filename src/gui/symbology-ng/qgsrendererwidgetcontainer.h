f9eaf128ffaa0f60ea2f362dd2cbf0cd394bf563
Nathan Woodrow <nathan_woodrow@technologyonecorp.com>
2016-06-09 07:20:13 +1000

    Doc

--- src/gui/symbology-ng/qgsrendererwidgetcontainer.h
+++ src/gui/symbology-ng/qgsrendererwidgetcontainer.h
@@ -22,45 +22,71 @@
 
 
 /**
- * @brief A container widget that can be used to show a renderer widget with a title and close button.
- * @note Mainly used for the style dock panels at the moment.
+ * @brief Base class for any widget that can be shown as a inline panel
  */
 class GUI_EXPORT QgsPanelWidget : public QWidget
 {
     Q_OBJECT
   public:
     /**
-     * @brief A container widget that can be used to show a renderer widget with a title and close button.
-     * @param widget The internal widget to be shown to the user.
-     * @param title The title to show on the widget.
-     * @param parent The parent of the widget.
+     * @brief Base class for any widget that can be shown as a inline panel
+     * @param parent Parent widget.
      */
     QgsPanelWidget(QWidget *parent = 0 );
 
+    /**
+     * Set the title of the panel when shown in the interface.
+     * @param panelTitle The panel title.
+     */
     void setPanelTitle( QString panelTitle ) { mPanelTitle = panelTitle; }
+
+    /**
+     * The title of the panel.
+     * @return The title pf the panel.
+     */
     QString panelTitle() { return mPanelTitle; }
 
   signals:
-    /**
-      * @brief Emitted when the container is accpeted and closed.
-      * Listen to this to clean up the callers state.
-      */
-    void panelAccepted( QgsPanelWidget* container );
-
-    void showPanel( QgsPanelWidget* container );
 
     /**
-     * Emiited when the internal widget changes state.
+     * Emiited when the panel is accpeted by the user.
+     * @param panel The panel widget that was accepted.
+     * @note This argument is normally raised with emit panelAccpeted(this)
+     * so that callers can retrive the widget easier in calling code.
      */
-    void widgetChanged( QgsPanelWidget* conatiner );
+    void panelAccepted( QgsPanelWidget* panel );
+
+    /**
+     * Emit when you require a panel to be show in the interface.
+     * @param panel The panel widget to show.
+     * @note If you are connected to this signal you should also connect
+     * given panels showPanel signal as they can be nested.
+     */
+    void showPanel( QgsPanelWidget* panel );
+
+    /**
+     * Emiited when the widget state changes.
+     * Connect to this to pull any changes off the widget when needed.
+     * As panels are non blocking "dialogs" you should listen to this signal
+     * to give the user feedback when something changes.
+     * @param panel The panels whos state changed.
+     * @note You can raise this easier from your own widget using emitWidgetChanged()
+     * which emits the signal as emit widgetChanged(this)
+     */
+    void widgetChanged( QgsPanelWidget* panel );
 
   public slots:
 
     /**
-     * Accept the container. Causes accepted to be emiited.
+     * Accept the panel. Causes panelAccepted to be emiited.
+     * Widgets are normally removed form the interface using the panel manager or the caller.
      */
     void acceptPanel();
 
+    /**
+     * EMit the widgetChanged signal for this widget.
+     * @note The same as doing emit widgetChanged(this);
+     */
     void emitWidgetChanged();
 
   protected:
@@ -76,11 +102,23 @@ class GUI_EXPORT QgsPanelWidget : public QWidget
 
 };
 
+
+/**
+ * A non model panel page that can get shown to the user.  Page will contain a close button and trigger the
+ * internal widgets acceptPanel() on close.
+ */
 class GUI_EXPORT QgsPanelWidgetPage : public QgsPanelWidget, private Ui::QgsRendererWidgetContainerBase
 {
     Q_OBJECT
   public:
+
+  /**
+    * A non model panel page that can get shown to the user.
+   * @param widget The internal widget to show in the page.
+   * @param parent THe parent widget.
+   */
   QgsPanelWidgetPage( QgsPanelWidget* widget, QWidget* parent = nullptr );
+
   ~QgsPanelWidgetPage();
 private:
   QWidget* mWidget;
